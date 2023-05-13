235cfe401c9098c79616f76b4f6cc41bc3795bfe
DenGuleDansker <chilinhm@gmail.com>
2023-05-13 04:35:06 +0200

    Brugerlisten er næsten færdig, skal bruge mService

--- Server/Repositories/BrugerRepositorySQL.cs
+++ Server/Repositories/BrugerRepositorySQL.cs
@@ -38,9 +38,9 @@ namespace Server.Repositories
                         var Telefonnummer = reader.GetInt32(3);
                         var Adresse = reader.GetString(4);
                         var Email = reader.GetString(5);
-                        var Fødselsdag = reader.GetDateTime(6);
-                        var Password = reader.GetString(7);
-                        var Iskoordinator = reader.GetBoolean(8);
+                        var Password = reader.GetString(6);
+                        var Iskoordinator = reader.GetBoolean(7);
+                        var Fødselsdag = DateTime.Parse(reader.GetString(8).Replace(".", "/").Remove(10, 9)); // Fjerner tidspunkt og erstatter . med / så strengen kan konverteres til en dato
 
                         Bruger b = new Bruger
                         {
@@ -64,20 +64,25 @@ namespace Server.Repositories
         public void Add(Bruger bruger)
         {
 
+
+
             using (var connection = new NpgsqlConnection(connectionString))
             {
+
+                string datoString = bruger.Fødselsdag.Date.ToString();
+
                 connection.Open();
                 var command = connection.CreateCommand();
 
-                command.CommandText = "INSERT INTO \"Bruger\" (\"Fornavn\", \"Efternavn\", \"Telefonnummer\", \"Adresse\", \"Email\", \"Fødselsdag\", \"Password\", \"isKoordinator\") VALUES (\'@Fornavn\', \'@Efternavn\', @Telefonnummer, \'@Adresse\', \'@Email\', \'@Fødselsdag\', \'@Password\', @isKoordinator)";
+                command.CommandText = "INSERT INTO \"Bruger\" (\"Fornavn\", \"Efternavn\", \"Telefonnummer\", \"Adresse\", \"Email\", \"Fødselsdag\", \"Password\", \"isKoordinator\") VALUES (@Fornavn, @Efternavn, @Telefonnummer, @Adresse, @Email, @Fødselsdag, @Password, @isKoordinator)";
                 command.Parameters.AddWithValue("@Fornavn", bruger.Fornavn);
                 command.Parameters.AddWithValue("@Efternavn", bruger.Efternavn);
                 command.Parameters.AddWithValue("@Telefonnummer", bruger.Telefonnummer);
                 command.Parameters.AddWithValue("@Adresse", bruger.Adresse);
                 command.Parameters.AddWithValue("@Email", bruger.Email);
-                command.Parameters.AddWithValue("@Fødselsdag", bruger.Fødselsdag);
+                command.Parameters.AddWithValue("@Fødselsdag", datoString);
                 command.Parameters.AddWithValue("@Password", bruger.Password);
-                command.Parameters.AddWithValue("isKoordinator", bruger.IsKoordinator);
+                command.Parameters.AddWithValue("@isKoordinator", bruger.IsKoordinator);
                 command.ExecuteNonQuery();
             }
         }
