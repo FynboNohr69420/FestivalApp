1fcf660b1e65634e50980d11e55cb18bb2307e3a
Marcus <eaamarbb@students.eaaa.dk>
2023-05-15 13:27:07 +0200

    hh

--- Server/Repositories/VagtRepositorySQL.cs
+++ Server/Repositories/VagtRepositorySQL.cs
@@ -4,6 +4,7 @@ using Dapper;
 using Npgsql;
 using Microsoft.Data.SqlClient;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+using MongoDB.Driver.Core.Configuration;
 
 namespace Server.Repositories
 {
@@ -119,11 +120,64 @@ namespace Server.Repositories
         {
             using (var connection = new NpgsqlConnection(connectionString))
             {
+                string datoString = vagt.Start.Date.ToString();
+                string datoString2 = vagt.Slut.Date.ToString();
+
                 connection.Open();
                 var command = connection.CreateCommand();
 
-
+                command.CommandText = "UPDATE \"Vagt\" SET \"Navn\"=@Navn, \"Point\"=@Point, \"Start\"=@Start, \"Slut\"=@Slut, \"Beskrivelse\"=@Beskrivelse, \"Kategori_ID\"=@Kategori, \"Antal_Pladser\"=Antal WHERE \"ID\" = @Id";
+                command.Parameters.AddWithValue("@Navn", vagt.Navn);
+                command.Parameters.AddWithValue("@Point", vagt.Point);
+                command.Parameters.AddWithValue("@Start", datoString);
+                command.Parameters.AddWithValue("@Slut", datoString2);
+                command.Parameters.AddWithValue("@Beskrivelse", vagt.Beskrivelse);
+                command.Parameters.AddWithValue("@Kategori_ID", vagt.Kategori );
+                command.Parameters.AddWithValue("@Antal_Pladser", vagt.Antal);
+                command.Parameters.AddWithValue("@ID", vagt.ID);
+                command.ExecuteNonQuery();
             }
         }
+
+        public Vagt GetVagt(int VagtID)
+        {
+            var result = new Vagt();
+            using (var connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                var command = connection.CreateCommand();
+                command.CommandText = "SELECT * FROM \"Vagt\" WHERE \"ID\" =" + "'" + VagtID + "'";
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var Id = reader.GetInt32(0);
+                        var Navn = reader.GetString(1);
+                        var Point = reader.GetInt32(2);
+                        var Start = DateTime.Parse(reader.GetString(3).Replace(".", "/").Remove(10, 9));
+                        var Slut = DateTime.Parse(reader.GetString(4).Replace(".", "/").Remove(10, 9));
+                        var Beskrivelse = reader.GetString(5);
+                        var KategoriID = reader.GetInt32(6);
+                        var Antal_Pladser = reader.GetInt32(7);
+
+                        Vagt b = new Vagt
+                        {
+                            ID = Id,
+                            Navn = Navn,
+                            Kategori = KategoriID,
+                            Point = Point,
+                            Start = Start,
+                            Slut = Slut,
+                            Antal = Antal_Pladser,
+                            Beskrivelse = Beskrivelse,
+                        };
+                        result = b;
+                    }
+                }
+            }
+            return result;
+        }
     }
+
 }
