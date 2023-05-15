d9b20c678b85e938f936d955d47c28b402b53db9
DenGuleDansker <chilinhm@gmail.com>
2023-05-15 14:32:02 +0200

    ændring

--- Client/Services/BrugerService.cs
+++ Client/Services/BrugerService.cs
@@ -34,8 +34,6 @@ namespace Client.Services
         {
             await http.PostAsJsonAsync<Bruger>("https://localhost:7004/api/brugere", bruger); // Sender en POST request med booking som JSON payload til API'en
             Console.WriteLine("klient: add " + bruger.Fornavn + bruger.Efternavn); // Udskriver informationer om den nye booking i konsollen//
-            bruger = new();
-
         }
 
         public async Task UpdateBruger(Bruger bruger)
