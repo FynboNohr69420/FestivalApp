5226a46bc4345117742b86fb8d2f37ad49df56b0
Marcus <eaamarbb@students.eaaa.dk>
2023-05-24 11:49:37 +0200

    Kommentar

--- Client/Services/BrugerService.cs
+++ Client/Services/BrugerService.cs
@@ -1,55 +1,53 @@
 ﻿using System;
 using System.Net.Http;
 using System.Net.Http.Json;
-using Amazon.Runtime.Internal.Endpoints.StandardLibrary;
 using Client.Pages;
 using Common.Model;
 using Microsoft.AspNetCore.Components;
-using static System.Net.WebRequestMethods;
 
 namespace Client.Services
 {
-    public class BrugerService : IBrugerService
+    public class BrugerService : IBrugerService // Klassen arver fra IBrugerService interfacet 
     {
         HttpClient http;
-        public BrugerService(HttpClient http)
+        public BrugerService(HttpClient http) // Sørger for at klassen kan kalde og hente Http requests 
         {
             this.http = http;
         }
 
-        public async Task<IEnumerable<Bruger>> getAll()
+        public async Task<IEnumerable<Bruger>> getAll() // getAll laver en http get request til /api/brugere og henter alle informationer på alle brugere
         {
             var brugerlist = await http.GetFromJsonAsync<Bruger[]>("https://localhost:7004/api/brugere");
             return brugerlist;
         }
 
-        public async Task<Bruger> GetBruger(int id)
+        public async Task<Bruger> GetBruger(int id) // Henter information på den enkelte bruger, baseret på deres id
         {
             return await http.GetFromJsonAsync<Bruger>($"https://localhost:7004/api/brugere/bruger/{id}");
         }
 
-        public async Task Add(Bruger bruger)
+        public async Task Add(Bruger bruger) // Tilføjer en bruger 
         {
             await http.PostAsJsonAsync<Bruger>("https://localhost:7004/api/brugere", bruger); // Sender en POST request med booking som JSON payload til API'en
             Console.WriteLine("klient: add " + bruger.Fornavn + bruger.Efternavn); // Udskriver informationer om den nye booking i konsollen//
         }
 
-        public async Task UpdateBruger(Bruger bruger)
+        public async Task UpdateBruger(Bruger bruger) // Opdater en bruger 
         {
             await http.PostAsJsonAsync<Bruger>("https://localhost:7004/api/brugere/update", bruger);
             Console.WriteLine("klient: add " + bruger.Fornavn + bruger.Efternavn); 
             bruger = new();
         }
 
-        public async Task<Bruger> getSpecific(string email)
+        public async Task<Bruger> getSpecific(string email) // Henter en specifik bruger baseret på email
         {
             var result = await http.GetFromJsonAsync<Bruger>("https://localhost:7004/api/brugere/" + email);
 
             return result;
         }
-        public void DeleteBruger(int ID)
+        public void DeleteBruger(int ID) // Sletter en bruger baseret på et specifikt ID
         {
-            http.DeleteFromJsonAsync<Vagt>($"https://localhost:7004/api/brugere/{ID}");
+            http.DeleteFromJsonAsync<Bruger>($"https://localhost:7004/api/brugere/{ID}");
             Console.WriteLine("Klient: deleted" +  ID);
         }
 
